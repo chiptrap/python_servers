1d07efe35020421c192e369657a72aef9f168f6b
Bricen Chitty <bricenchitty@gmail.com>
2025-03-09 14:51:57 -0700

    Started adding netcat utilites and got mini working tcp_client and tcp_server

--- tcp_client.py
+++ tcp_client.py
@@ -1,7 +1,7 @@
 import socket
 
-terget_host = "www.google.com"
-target_port = 80
+target_host = "127.0.0.1"
+target_port = 9998
 
 # create a socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,4 +10,10 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((target_host,target_port))
 
 # send some data
-client.send("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+
+# receive some data
+response = client.recv(4096)
+
+print(response.decode())
+client.close()
